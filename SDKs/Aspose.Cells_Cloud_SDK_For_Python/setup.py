e861c2c624fcd6bf35bf6972e791ac0e09b562bb
Farooq Sheikh <farooq.sheikh@aspose.com>
2015-09-22 13:44:50 +0500

    add package dependency

--- SDKs/Aspose.Cells_Cloud_SDK_For_Python/setup.py
+++ SDKs/Aspose.Cells_Cloud_SDK_For_Python/setup.py
@@ -9,7 +9,10 @@ setup(
     description = 'Aspose.Cells Cloud SDK for Python allows you to use Aspose.Cells APIs in your Python applications',
     author='Farooq Sheikh',
     author_email='farooq.sheikh@aspose.com',
-    url='',
+    url='https://github.com/asposecells/Aspose_Cells_Cloud/tree/master/SDKs/Aspose.Cells_Cloud_SDK_For_Python',
+    install_requires=[
+        'asposestoragecloud',
+    ],
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
