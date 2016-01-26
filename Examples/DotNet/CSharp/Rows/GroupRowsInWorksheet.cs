031753bc007b9ad4a41c9c14f1a135f1bb01aa19
Saqib Masood <saqib@masood.pk>
2016-01-26 17:30:22 +0500

    Remove copyright notice from source files

--- Examples/DotNet/CSharp/Rows/GroupRowsInWorksheet.cs
+++ Examples/DotNet/CSharp/Rows/GroupRowsInWorksheet.cs
@@ -1,12 +1,4 @@
-﻿//////////////////////////////////////////////////////////////////////////
-// Copyright 2001-2015 Aspose Pty Ltd. All Rights Reserved.
-//
-// This file is part of Aspose.Imaging. The source code in this file
-// is only intended as a supplement to the documentation, and is provided
-// "as is", without warranty of any kind, either expressed or implied.
-//////////////////////////////////////////////////////////////////////////
-
-using Aspose.Cloud;
+﻿using Aspose.Cloud;
 using System;
 namespace Aspose.Cells.Cloud.Examples.Rows
 {
--- Examples/DotNet/CSharp/Workbook/ConvertToAnotherFormat.cs
+++ Examples/DotNet/CSharp/Workbook/ConvertToAnotherFormat.cs
@@ -1,12 +1,4 @@
-﻿//////////////////////////////////////////////////////////////////////////
-// Copyright 2001-2015 Aspose Pty Ltd. All Rights Reserved.
-//
-// This file is part of Aspose.Imaging. The source code in this file
-// is only intended as a supplement to the documentation, and is provided
-// "as is", without warranty of any kind, either expressed or implied.
-//////////////////////////////////////////////////////////////////////////
-
-using Aspose.Cloud;
+﻿using Aspose.Cloud;
 using System;
 namespace Aspose.Cells.Cloud.Examples.Workbook
 {
@@ -20,11 +12,19 @@ namespace Aspose.Cells.Cloud.Examples.Workbook
             string output = "output.pdf";
             string outPath = "cellsOut/";
             
-           // Common.StorageService.File.UploadFile(dataDir + input, input, storage: Common.STORAGE);
+           
+
+
+
+ Common.StorageService.File.UploadFile(dataDir + input, input, storage: Common.STORAGE);
             
             Common.CellsService.Workbook.ConvertWorkbookToSomeFormat(WorkbookExportFormat.Pdf, string.Empty, outPath+output, dataDir+input);
             
-            //Common.StorageService.File.DownloadFile(outPath, dataDir + output, storage: Common.STORAGE);
+            
+
+
+
+Common.StorageService.File.DownloadFile(outPath, dataDir + output, storage: Common.STORAGE);
             
             Common.StorageService.File.DownloadFile(outPath+output, dataDir+output);
         }
