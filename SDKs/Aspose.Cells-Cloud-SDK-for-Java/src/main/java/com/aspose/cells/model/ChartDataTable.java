254a5ee075b6ef6c8efb24b88ce3d0423fca5c26
Farooq Sheikh <farooq.sheikh@aspose.com>
2016-06-21 16:47:31 +0500

    Revamp SDK Folder Structure

---
+++ SDKs/Aspose.Cells-Cloud-SDK-for-Java/src/main/java/com/aspose/cells/model/ChartDataTable.java
@@ -0,0 +1,15 @@
+package com.aspose.cells.model;
+
+public class ChartDataTable {
+
+   private Link link = null;
+
+   public Link getLink() {
+      return link;
+   }
+
+   public void setLink(Link link) {
+      this.link = link;
+   }
+   
+}
---
+++ SDKs/Aspose.Cells-Cloud-SDK-for-Java/src/main/java/com/aspose/cells/model/SideWall.java
@@ -0,0 +1,14 @@
+package com.aspose.cells.model;
+
+public class SideWall {
+   private Link link = null;
+
+   public Link getLink() {
+      return link;
+   }
+
+   public void setLink(Link link) {
+      this.link = link;
+   }
+   
+}
