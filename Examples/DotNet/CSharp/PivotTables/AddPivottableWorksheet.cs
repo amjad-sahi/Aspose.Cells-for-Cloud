8f77e7b6a6305d0a72269377c61e2fe47bba50d8
Aspose.Slides Product Family <naeem.akram@aspose.com>
2017-02-02 11:12:37 +0500

    Update AddPivottableWorksheet

--- Examples/DotNet/CSharp/PivotTables/AddPivottableWorksheet.cs
+++ Examples/DotNet/CSharp/PivotTables/AddPivottableWorksheet.cs
@@ -13,23 +13,34 @@ namespace PivotTables
             CellsApi cellsApi = new CellsApi(Common.APP_KEY, Common.APP_SID, Common.BASEPATH);
             StorageApi storageApi = new StorageApi(Common.APP_KEY, Common.APP_SID, Common.BASEPATH);
 
-            String fileName = "Sample_OleObject_Book1.xlsx";
-            String sheetName = "Sheet1";
-            int objectNumber = 0;
-            String storage = "";
-            String folder = "";
+            String fileName = "Sample_Test_Book.xls";
+            string sheetName = "Sheet1";
+            string storage = null;
+            string folder = null;
+            string sourceData = null;
+            string destCellName = null;
+            string tableName = null;
+            bool? useSameSource = false;
+            Com.Aspose.Cells.Model.CreatePivotTableRequest body = new Com.Aspose.Cells.Model.CreatePivotTableRequest();
+            body.Name = "MyPivot";
+            body.SourceData = "A5:E10";
+            body.DestCellName = "H20";
+            body.UseSameSource = true;
+            body.PivotFieldRows = new System.Collections.Generic.List<int?> { 1 };
+            body.PivotFieldColumns = new System.Collections.Generic.List<int?> { 1 };
+            body.PivotFieldData = new System.Collections.Generic.List<int?> { 1 };
 
             try
             {
                 // Upload source file to aspose cloud storage
                 storageApi.PutCreate(fileName, "", "", System.IO.File.ReadAllBytes(Common.GetDataDir() + fileName));
 
-                // Invoke Aspose.Cells Cloud SDK API to get OleObject from worksheet
-                ResponseMessage apiResponse = cellsApi.GetWorksheetOleObject(fileName, sheetName, objectNumber, storage, folder);
+                // Invoke Aspose.Cells Cloud SDK API to add pivot table worksheet
+                PivotTableResponse apiResponse = cellsApi.PutWorksheetPivotTable(fileName, sheetName, storage, folder, sourceData, destCellName, tableName, useSameSource, body);
 
-                if (apiResponse != null)
+                if (apiResponse != null && apiResponse.Status.Equals("OK"))
                 {
-                    Console.WriteLine("OleObject" + System.Text.Encoding.Default.GetString(apiResponse.ResponseStream));
+                    Console.WriteLine("Add a Pivot Table in a Worksheet, Done!");
                     Console.ReadKey();
                 }
             }
