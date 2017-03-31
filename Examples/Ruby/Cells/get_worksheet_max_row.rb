99ed890ba1c56397933ae8f55e68e462ab20c45a
SOHAIL <msohailismail@gmail.com>
2017-03-31 10:56:42 +0500

    Cells Examples

---
+++ Examples/Ruby/Cells/get_worksheet_max_row.rb
@@ -0,0 +1,33 @@
+#ExStart:
+require 'aspose_cells_cloud'
+
+class Cell
+
+  include AsposeCellsCloud
+  include AsposeStorageCloud
+
+  def initialize
+    #Get App key and App SID from https://cloud.aspose.com
+    AsposeApp.app_key_and_sid("APP_KEY", "APP_SID")
+    @cells_api = CellsApi.new  
+  end
+
+  def upload_file(file_name)
+    @storage_api = StorageApi.new 
+    response = @storage_api.put_create(file_name, File.open("../../../data/" << file_name,"r") { |io| io.read } )
+  end
+
+  def get_worksheet_max_row
+      file_name = "Sample_Test_Book.xls"
+      upload_file(file_name)
+      
+      sheet_name = "Sheet2"
+      cell_or_method_name = "maxrow"
+      response = @cells_api.get_worksheet_cell(file_name, sheet_name, cell_or_method_name)
+  end
+
+end
+
+cell = Cell.new()
+puts cell.get_worksheet_max_row
+#ExEnd:
